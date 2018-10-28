afc37ebb936493b24682f83d7a06dd3c56efc54a
Melinda Hernandez <melindahernandez@melindas-mbp.lan>
2018-10-28 12:03:23 -0500

    Updates

---
+++ precache-manifest.b7d01a98d22ae87bdf9a7e9b46d169b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "95dc9e5e96ebf57b69c0",
+    "url": "/weather-app/static/css/main.61561769.chunk.css"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "c029d3c2e594d4f6d188",
+    "url": "/weather-app/static/js/1.c029d3c2.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "95dc9e5e96ebf57b69c0",
+    "url": "/weather-app/static/js/main.95dc9e5e.chunk.js"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "c029d3c2e594d4f6d188",
+    "url": "/weather-app/static/css/1.e3bc389e.chunk.css"
+  },
+  {
+    "revision": "70193ee44e354f67f2a4b8ac9be060f4",
+    "url": "/weather-app/index.html"
+  }
+];
